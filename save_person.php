09896c77b7864a91bd11b8d3d23cb440976f38d9
Remy PERRET <reperret@hotmail.com>
2025-05-06 17:14:46 +0200

    modif bdd

--- save_person.php
+++ save_person.php
@@ -1,6 +1,9 @@
 <?php
 try {
+    // acces bdd
     $pdo = new PDO('mysql:host=localhost;dbname=reppop', 'root', 'Deflagratione89!');
+
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
